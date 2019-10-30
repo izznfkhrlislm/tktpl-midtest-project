30737b275e04bbb9d524fec8fe652dbea6edfa1e
Izzan Fakhril Islam <izzan.fakhril@ui.ac.id>
2019-10-30 21:24:32 +0700

    Fixing multi fragment logics

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/izzanfi/phoneinformer/DetailActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/izzanfi/phoneinformer/DetailActivity.java
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.CpuInformationFragment;
 import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.DetailFragment;
+import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.PhoneInformationFragment;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -17,12 +19,26 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         Intent detailActivityIntent = getIntent();
-        String detailTitle = detailActivityIntent.getStringExtra("title");
-        String detailDesc = detailActivityIntent.getStringExtra("desc");
+        String title = detailActivityIntent.getStringExtra("title");
+        String description = detailActivityIntent.getStringExtra("desc");
 
         FragmentManager detailActivityFm = getSupportFragmentManager();
         FragmentTransaction detailActivityFt = detailActivityFm.beginTransaction();
-        detailActivityFt.replace(R.id.detailFragment, DetailFragment.newInstance(detailTitle, detailDesc));
+
+        if (title.equals("Phone Information")) {
+            detailActivityFt.replace(R.id.detailFragment, PhoneInformationFragment.newInstance(title, description));
+        } else if (title.equals("CPU Information")) {
+            detailActivityFt.replace(R.id.detailFragment, CpuInformationFragment.newInstance(title, description));
+        } else if (title.equals("Batery Information")) {
+            detailActivityFt.replace(R.id.detailFragment, PhoneInformationFragment.newInstance(title, description));
+        } else if (title.equals("System Information")) {
+            detailActivityFt.replace(R.id.detailFragment, PhoneInformationFragment.newInstance(title, description));
+        } else if (title.equals("Settings")) {
+            detailActivityFt.replace(R.id.detailFragment, PhoneInformationFragment.newInstance(title, description));
+        } else if (title.equals("About")) {
+            detailActivityFt.replace(R.id.detailFragment, PhoneInformationFragment.newInstance(title, description));
+        }
+
         detailActivityFt.commit();
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/izzanfi/phoneinformer/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/izzanfi/phoneinformer/MainActivity.java
@@ -3,13 +3,14 @@ package id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.View;
 
-import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.DetailFragment;
+import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.CpuInformationFragment;
 import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.MenuFragment;
 import id.ac.ui.cs.mobileprogramming.izzanfi.phoneinformer.fragments.PhoneInformationFragment;
 
@@ -43,26 +44,31 @@ public class MainActivity extends AppCompatActivity implements Communicable {
         Log.d(TAG, "title: " + title);
         Log.d(TAG, "mIsDualPane: " + mIsDualPane);
         if (mIsDualPane) {
+            for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+                if (fragment instanceof MenuFragment) {
+                    continue;
+                } else {
+                    getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+                }
+            }
+
             //TODO: Implement dual pane view (for landscape)
             FragmentManager mainActivityFm = getSupportFragmentManager();
             FragmentTransaction mainActivityFt = mainActivityFm.beginTransaction();
 
-            switch (title) {
-                case "Phone Information":
-                    mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
-                case "CPU Information":
-                    mainActivityFt.replace(R.id.detailMenuFragment, DetailFragment.newInstance(title, description));
-                case "Battery Information":
-                    mainActivityFt.replace(R.id.detailMenuFragment, DetailFragment.newInstance(title, description));
-                case "System Information":
-                    mainActivityFt.replace(R.id.detailMenuFragment, DetailFragment.newInstance(title, description));
-                case "Settings":
-                    mainActivityFt.replace(R.id.detailMenuFragment, DetailFragment.newInstance(title, description));
-                case "About":
-                    mainActivityFt.replace(R.id.detailMenuFragment, DetailFragment.newInstance(title, description));
+            if (title.equals("Phone Information")) {
+                mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
+            } else if (title.equals("CPU Information")) {
+                mainActivityFt.replace(R.id.detailMenuFragment, CpuInformationFragment.newInstance(title, description));
+            } else if (title.equals("Batery Information")) {
+                mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
+            } else if (title.equals("System Information")) {
+                mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
+            } else if (title.equals("Settings")) {
+                mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
+            } else if (title.equals("About")) {
+                mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
             }
-
-//            mainActivityFt.replace(R.id.detailMenuFragment, PhoneInformationFragment.newInstance(title, description));
 
             mainActivityFt.commit();
 
